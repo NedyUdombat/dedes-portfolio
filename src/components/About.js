faf39d04e94bd92e37fbbedaee28cd088655938c
nedyudombat <nedyudombat@gmail.com>
2020-10-24 01:58:30 +0100

    linter setup

--- src/components/About.js
+++ src/components/About.js
@@ -6,29 +6,30 @@ import Avatar from '../assets/images/avatar.png';
 
 class About extends Component {
   render() {
-
     return (
       <header id="about">
-      <section className="row">
-         <div className="img-div">
-           <img src={Avatar} class="headshot" alt="Nnamdi's Head shot" />
-         </div>
+        <section className="row">
+          <div className="img-div">
+            <img src={Avatar} class="headshot" alt="Nnamdi's Head shot" />
+          </div>
 
-         <div className="details">
-            <h2>
-               Who Am I?
-            </h2>
+          <div className="details">
+            <h2>Who Am I?</h2>
 
             <p>
-               Well, my name is Nnamdi Okafor. I am a 24-year-Old Growth Marketer.  I currently work at Bundle as a Growth Analyst. I spend most of my time thinking of Growth models, acquisition channels, activation and the entire Growth funnel, I am a firm believer of Product led growth. When I am not working, I am either cracking dry jokes or involved in some football banter(PS: I am a shameless Arsenal fan).
-
-I am currently the Facilitator of the Growth Academy. A free program designed to train 20 bright minds into becoming the next-gen of Growth marketers.
-
+              Well, my name is Nnamdi Okafor. I am a 24-year-Old Growth
+              Marketer. I currently work at Bundle as a Growth Analyst. I spend
+              most of my time thinking of Growth models, acquisition channels,
+              activation and the entire Growth funnel, I am a firm believer of
+              Product led growth. When I am not working, I am either cracking
+              dry jokes or involved in some football banter(PS: I am a shameless
+              Arsenal fan). I am currently the Facilitator of the Growth
+              Academy. A free program designed to train 20 bright minds into
+              becoming the next-gen of Growth marketers.
             </p>
-         </div>
-      </section>
-
-   </header>
+          </div>
+        </section>
+      </header>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ParticlesBg  from "particles-bg";
+import ParticlesBg from 'particles-bg';
 
 import '../assets/stylesheet/header.css';
 
@@ -7,34 +7,59 @@ import '../assets/stylesheet/header.css';
 
 class Header extends Component {
   render() {
-
     return (
       <header id="home">
-      <ParticlesBg type="thick" bg={true} num={10}  />
-      <nav id="nav-wrap">
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
-         </ul>
-      </nav>
+        <ParticlesBg type="thick" bg={true} num={10} />
+        <nav id="nav-wrap">
+          <ul id="nav" className="nav">
+            <li className="current">
+              <a className="smoothscroll" href="#home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#about">
+                About
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#resume">
+                Resume
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#portfolio">
+                Works
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
 
-      <div className="row banner">
-         <div className="banner-text">
-           {/* <img src={Logo} class="logo" alt="Nnamdi's Logo" /> */}
-            <h1 className="responsive-headline">Hi, I am glad you found your way here :) </h1>
-            <h3>Hi, I am Nnamdi Okafor. I am a 24-year-Old Growth Marketer.  I currently work at Bundle Africa as a Growth Analyst</h3>
+        <div className="row banner">
+          <div className="banner-text">
+            {/* <img src={Logo} class="logo" alt="Nnamdi's Logo" /> */}
+            <h1 className="responsive-headline">
+              Hi, I am glad you found your way here :){' '}
+            </h1>
+            <h3>
+              Hi, I am Nnamdi Okafor. I am a 24-year-Old Growth Marketer. I
+              currently work at Bundle Africa as a Growth Analyst
+            </h3>
             <hr />
-         </div>
-      </div>
+          </div>
+        </div>
 
-      <p className="scrolldown">
-         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-      </p>
-
-   </header>
+        <p className="scrolldown">
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle"></i>
+          </a>
+        </p>
+      </header>
     );
   }
 }
